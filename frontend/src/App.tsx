221e46b5a90fe3ea26a336356e21376700d828d9
diegocontinta <hola@diegocontinta.com>
2026-07-23 14:03:46 -0400

    chore: initialize project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+        return (
+                <main> 
+                       <h1>TCIT - POSTS</h1>
+                       <p>health: Ok</p>
+                </main>
+        )
+}
